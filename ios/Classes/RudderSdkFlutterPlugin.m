b306f184fee5a27ab54b1d1b08c5fd37463e9a9b
desusai <saivenkatdesu@gmail.com>
2021-02-04 23:35:51 +0530

    Initial Push

---
+++ ios/Classes/RudderSdkFlutterPlugin.m
@@ -0,0 +1,20 @@
+#import "RudderSdkFlutterPlugin.h"
+
+@implementation RudderSdkFlutterPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"rudder_sdk_flutter"
+            binaryMessenger:[registrar messenger]];
+  RudderSdkFlutterPlugin* instance = [[RudderSdkFlutterPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
